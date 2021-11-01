719fc1a9ee27a604ac9d5cba80e7f9f1887af678
Wiris R J Wernek <rafaeljwerneck43@gmail.com>
2021-11-01 12:14:02 -0300

    Adicionado condiçao de fim de jogo

--- script.js
+++ script.js
@@ -59,6 +59,13 @@ function iniciarJogo() {
     snake[0].y = 16 * box;
   }
 
+  for (i = 1; i < snake.length; i++) {
+    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+      clearInterval(jogo);
+      alert("End of Game");
+    }
+  }
+
   criarBG();
   criarCobrinha();
   drawFood();
